fc072f28c99cc1ebf19537ce8adb5efc4ec3b8fc
TePoYe <TePoYe@users.noreply.github.com>
2026-07-27 22:14:55 +0700

    Khoi tao bien sheets

--- index.js
+++ index.js
@@ -173,6 +173,15 @@ app.post('/api/run-test', async (req, res) => {
     let browser;
     
     try {
+        const { google } = require('googleapis'); // (Nếu trên cùng của file chưa có dòng này)
+        const auth = new google.auth.GoogleAuth({
+        credentials: {
+            client_email: process.env.GOOGLE_CLIENT_EMAIL,
+            private_key: process.env.GOOGLE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+        },
+        scopes: ['https://www.googleapis.com/auth/spreadsheets'],
+    });
+    const sheets = google.sheets({ version: 'v4', auth });
         // Đọc dữ liệu từ Google Sheet
         const sheetName = 'Trang tính1';
         const response = await sheets.spreadsheets.values.get({
